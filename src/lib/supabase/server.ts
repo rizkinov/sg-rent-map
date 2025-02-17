2fe06cd030e34d22aeac177555c5b1cfd2e779f6
rizkinov <148356406+rizkinov@users.noreply.github.com>
2025-02-17 20:46:39 +0800

    Supabase Setup

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,4 +4,16 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://cnqdthislyimyffgewif.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNucWR0aGlzbHlpbXlmZmdld2lmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTY4MjY2OCwiZXhwIjoyMDU1MjU4NjY4fQ.tVjkKICtkKLU-JSg8M3LPpsyJu24NpwFudNjHxDDWqA'
 
-export const supabase = createClient(supabaseUrl, supabaseKey) 
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export function createServerClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: false
+      }
+    }
+  )
+} 
